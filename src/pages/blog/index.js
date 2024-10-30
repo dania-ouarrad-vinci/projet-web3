a86725b017fa9e4dba93f6bdaae78c5f4c68e3f6
Asmae Hammouten <asmae.hammouten@student.vinci.be>
2024-10-30 21:25:02 +0100

    add : style blog post

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,24 +1,50 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
+import { GatsbyImage,getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import {
+  cardContainer,
+  card,
+  cardTitle,
+  cardLink,
+  tag,
+} from './index.module.css'
+
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
+      <div className={cardContainer}>
+        
       {
-        data.allMdx.nodes.map(node => (
-          <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.frontmatter.slug}`}>
+        data.allMdx.nodes.map((node) => {
+          const image = getImage(node.frontmatter.hero_image);
+         
+          return (
+         <article key={node.id} className={card}>
+             <GatsbyImage
+                  image={image}
+                  alt={node.frontmatter.hero_image_alt}
+               />
+
+            <h2 className={cardTitle}>
+              <Link to={`/blog/${node.frontmatter.slug}`} className={cardLink}>
                 {node.frontmatter.title}
               </Link>
             </h2>
+
             <p>Posted: {node.frontmatter.date}</p>
+
             <p>{node.excerpt}</p>
+            <Link to={`/blog/${node.frontmatter.slug}`} className={cardLink}><span className={tag}>En savoir plus</span></Link>
+
           </article>
-        ))
-      }
+       );
+      })
+    }
+
+    </div>
     </Layout>
   )
 }
@@ -31,6 +57,13 @@ export const query = graphql`
           date(formatString: "MMMM D, YYYY")
           title
           slug
+
+          hero_image_alt
+          hero_image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
         }
         id
         excerpt
